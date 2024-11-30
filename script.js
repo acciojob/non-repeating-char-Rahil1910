4f22a429670fd52fb5979498bba025b633663613
Rahil1910 <rahilrafique1234@gmail.com>
2024-11-30 14:29:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let rep = false;
+	for(let i=0;i<str.length;i=i+2){
+		if(str[i]!==str[i+1]){
+			rep = true;
+			return str[i];
+		}
+	}
+	if(rep===false){
+		return null;
+	}
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
