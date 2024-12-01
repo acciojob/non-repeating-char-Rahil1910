f8be0b7cd44a4a3ec0a5678402270dc3f0f89f21
Rahil1910 <rahilrafique1234@gmail.com>
2024-12-01 12:43:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
-	let rep = false;
+	let rep = false;  
 	for(let i=0;i<str.length;i=i+2){
 		if(str[i]!==str[i+1]){
 			rep = true;
-			return str[i];
+			return str[i]; 
 		}
 	}
-	if(rep===false){
-		return null;
+	if(rep==false){
+		return "null";
 	}
 }
 const input = prompt("Enter a string");
